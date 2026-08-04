360453902fcdbf8c77c8b28238feb1aaa6bebeb0
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-08-04 19:04:51 +0800

    Update multi_upload3.sh

--- multi_upload3.sh
+++ multi_upload3.sh
@@ -65,7 +65,7 @@ echo "Files uploaded successfully."
 echo "Preparing to send Telegram notification..."
 
 RELEASE_TAG="$version"
-GITHUB_REPO="${GITHUB_REPO:-evolutiion_lgg6}"
+GITHUB_REPO="${GITHUB_REPO:-lg_releases}"
 declare -a FILE_ENTRIES
 
 for filename in "${filenames[@]}"; do

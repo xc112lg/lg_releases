d9f143b1260903e8490574b2623483b150e2b43b
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-08-10 14:54:02 +0800

    s

--- fixes.sh
+++ fixes.sh
@@ -37,3 +37,6 @@ curl -sL https://raw.githubusercontent.com/xc112lg/evolutiion_lgg6/refs/heads/ma
 curl -sL https://raw.githubusercontent.com/xc112lg/lg_releases/refs/heads/main/0001-battery-config-override-fixed.patch  | patch -d device/lge/g6-common -p1
 curl -sL https://raw.githubusercontent.com/xc112lg/lg_releases/refs/heads/main/0001-bcmdhd-fix-NULL-ndev-deref-in-wl_notify_pfn_status.patch  | patch -d kernel/lge/msm8996 -p1
 #curl -sL https://github.com/xc112lg/android_device_lge_g6-common/commit/89433a836be4dbc067d75ab631604039718322c3.patch | git -C device/lge/g6-common am
+
+cat device/lge/g6-common/overlay/frameworks/base/core/res/res/values/config.xml
+cat kernel/lge/msm8996/drivers/net/wireless/bcmdhd/src/wl/sys/wl_cfg80211.c

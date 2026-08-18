324d4dc329e3d032c16e636cea2493b6339c9675
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-08-19 05:33:41 +0800

    s

--- b.sh
+++ b.sh
@@ -1,1 +1,1 @@
-curl -sf https://raw.githubusercontent.com/xc112lg/lg_releases/refs/heads/main/d1.sh | bash -s evolution
+curl -sf https://raw.githubusercontent.com/xc112lg/lg_releases/refs/heads/main/d2.sh | bash -s evolution upload

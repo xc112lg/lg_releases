ba01df84ba421f87fece45c6aa0c202ecc02f4f1
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-09-07 08:50:49 +0800

    s

--- b.sh
+++ b.sh
@@ -1,1 +1,1 @@
-curl -sf https://raw.githubusercontent.com/xc112lg/lg_releases/refs/heads/main/d2.sh | bash -s evolution
+curl -sf https://raw.githubusercontent.com/xc112lg/lg_releases/refs/heads/main/a16.sh | bash -s crdroid

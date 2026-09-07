64db3750124aca8bbec1cda540e0cc5bc6e77d07
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-09-08 05:33:23 +0800

    Update fixes.sh

--- fixes.sh
+++ fixes.sh
@@ -8,14 +8,7 @@ LINUX_COMPILE_BY="stendro_+_AShiningRay_+_continued_by_xc112lg"' \
 LINUX_COMPILE_HOST="crave.io"' \
     "$KERNEL_DIR/scripts/mkcompile_h"
 fi
-sed -i \
-  -e '/<path name="headphones-hifi-dac">/a\        <ctl name="Es9218 Bypass" value="0" />' \
-  -e '/<path name="headphones-hifi-dac-advanced">/a\        <ctl name="Es9218 Bypass" value="0" />' \
-  -e '/<path name="headphones-hifi-dac-aux">/a\        <ctl name="Es9218 Bypass" value="0" />' \
-  -e '/<path name="headphones-hifi-dacdop">/a\        <ctl name="Es9218 Bypass" value="0" />' \
-  -e '/<path name="headphones-hifi-dacdop-advanced">/a\        <ctl name="Es9218 Bypass" value="0" />' \
-  -e '/<path name="headphones-hifi-dacdop-aux">/a\        <ctl name="Es9218 Bypass" value="0" />' \
-  device/lge/g6-common/audio/mixer_paths_tasha.xml
+
 
 mkdir -p device/lge/msm8996-common/sepolicy/vendor-user
 if [ ! -f device/lge/msm8996-common/sepolicy/vendor-user/file.te ]; then
@@ -32,7 +25,3 @@ EOF
 grep -q '^[[:space:]]*# props\.append("ro\.adb\.secure=1")' build/soong/scripts/gen_build_prop.py ||
 sed -i 's/^\([[:space:]]*\)props\.append("ro\.adb\.secure=1")/\1# props.append("ro.adb.secure=1")/' build/soong/scripts/gen_build_prop.py
 cat build/soong/scripts/gen_build_prop.py
-curl -sL https://raw.githubusercontent.com/xc112lg/evolutiion_lgg6/refs/heads/main/init.qcom.usb.rc.patch | patch -d device/lge/msm8996-common -p0
-#curl -sL https://raw.githubusercontent.com/xc112lg/lg_releases/refs/heads/main/0001-battery-config-override-fixed.patch  | patch -d device/lge/g6-common -p1
-#curl -sL https://raw.githubusercontent.com/xc112lg/lg_releases/refs/heads/main/0001-bcmdhd-fix-NULL-cfgdev-deref-in-wl_notify_pfn_status-and-wl_notify_mic_status.patch  | patch -d kernel/lge/msm8996 -p1
-#curl -sL https://github.com/xc112lg/android_device_lge_g6-common/commit/89433a836be4dbc067d75ab631604039718322c3.patch | git -C device/lge/g6-common am

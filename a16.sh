ee2e64cfe34610a775d92f78e50b0c794c93cbfe
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-09-18 02:42:13 +0800

    Update a16.sh

--- a16.sh
+++ a16.sh
@@ -268,6 +268,7 @@ run_lunaris() {
     fixesdev
         curl -sf https://raw.githubusercontent.com/xc112lg/lg_releases/refs/heads/main/blur.sh | bash
     sed -i "\$a ro.lunaris.maintainer=xc112lg | How's Your Day" device/xiaomi/blossom/system.prop
+    grep -q "OPTION_CHECK_BRACKETS" frameworks/base/core/java/android/database/sqlite/SQLiteTokenizer.java 2>/dev/null && echo "already patched, skipping" || curl -L https://github.com/Evolution-X/frameworks_base/commit/5fc391bd5ab762123490a83c405d0ed0e23b7802.patch | git -C frameworks/base am
      . build/envsetup.sh
 
     local devices=(${ROM_DEVICES[lunaris]})

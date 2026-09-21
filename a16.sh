f0670b0316d6202618377b0ecea4a6df2cf6329e
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-09-22 06:05:43 +0800

    Update a16.sh

--- a16.sh
+++ a16.sh
@@ -56,7 +56,7 @@ declare -A ROM_DEVICES=(
     [axion]="h872 h870 us997 h873 h870d"
     [crdroid]="h872 h870 us997 h873 h870d"
     #[crdroid]="h872 h870d"
-     [lunaris]="h850"
+     [lunaris]="h850 h872"
   # [evolution]="h830 h850"
 )
 
